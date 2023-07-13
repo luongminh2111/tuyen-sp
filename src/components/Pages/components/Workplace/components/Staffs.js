dbc78f98b124f840c688dac529b0364fc78cd48e
luongminh2111 <luongvanminhk63bk@gmail.com>
2023-07-13 23:51:39 +0700

    fix: user

--- src/components/Pages/components/Workplace/components/Staffs.js
+++ src/components/Pages/components/Workplace/components/Staffs.js
@@ -1,11 +1,89 @@
 import React from "react";
 import "../styles/Staff.scss";
+import { useState } from "react";
+import { ORG_IMAGE_DEFAULT } from "../../../../../commons/image";
+import { useHistory } from "react-router-dom";
+
 
 function Staffs(props) {
+
+  const history = useHistory();
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const renderEditUser = () => {
+    return (
+      <div className="add-user-content-wrapper">
+        <div className="title d-flex" style={{color: '#666', fontWeight: '600'}}>
+          <div>Edit User </div>
+          <div>
+            <i class="fa-sharp fa-solid fa-circle-question"></i>
+          </div>
+        </div>
+        <div className="back-to-edit" onClick={() => setIsEdit(false)} style={{marginTop: '12px', fontSize: '13px'}}>
+          <i class="fa-sharp fa-solid fa-arrow-left" style={{marginRight: '8px'}}></i>
+          Back
+        </div>
+        <hr />
+        <div className="add-user-content">
+          <div className="info">User Information</div>
+          <div className="add-item">
+            <div className="input-name">
+              <div className="label">
+                <span>Full name</span>
+                <span>*</span>
+              </div>
+              <div>
+                <input type="text" />
+              </div>
+            </div>
+            <div className="input-name mt-3">
+              <div className="label">
+                <span>Email Address</span>
+                <span>*</span>
+              </div>
+              <div>
+                <input type="text" />
+              </div>
+            </div>
+            <div className="input-name mt-3">
+              <div className="label">
+                <span>Avatar</span>
+                <span>*</span>
+              </div>
+              <div className="avatar">
+                <img src={ORG_IMAGE_DEFAULT} ></img>
+              </div>
+            </div>
+            <div className="input-name mt-3">
+              <div className="label">
+                <span>Role</span>
+                <span>*</span>
+              </div>
+              <div>
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center submit-btn">
+          <button>Add</button>
+        </div>
+      </div>
+    );
+  };
+
+  if (isEdit) {
+    return renderEditUser();
+  }
+
   return (
     <div className="staff-content-wrapper">
       <div className="title d-flex">
         <div>Staffs (4 members)</div>
+      </div>
+      <div className="btn-add-user">
+        <button onClick={() => setIsEdit(true)}>Add User</button>
       </div>
       <div className="filter-member d-flex">
         <div className="text-1">Filter staff</div>
@@ -26,7 +104,7 @@ function Staffs(props) {
         <div className="body">
           {[0, 1, 2, 3]?.map((e) => {
             return (
-              <div className="item">
+              <div className="item" onClick={() => history.push("/my-profile")}>
                 <div className="name">Vu Duc Tuyen</div>
                 <div className="mail">vutuyenkt2000@gmail.com</div>
                 <div className="role">Administrator</div>
